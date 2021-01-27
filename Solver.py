05d4b5ce631896c86ae32c5cce0ee1fb3c9fe985
Meeeek <conradfukuzawa@hotmail.ca>
2021-01-27 15:16:56 -0500

    Reworked Solver.init and Cell
    The Cell reworks remove i, and now only has num, this makes it less confusing
    Solver.init has been changed so that the element list is only of type Cell

--- Solver.py
+++ Solver.py
@@ -6,15 +6,9 @@ import cell
 class SudokuSolver:
 
     # this is a list of the columns, that way when calling it's x axis first
-    elements = [[0, 0, 0, 0, 0, 0, 0, 0, 0],\
-                [0, 0, 0, 0, 0, 0, 0, 0, 0],\
-                [0, 0, 0, 0, 0, 0, 0, 0, 0],\
-                [0, 0, 0, 0, 0, 0, 0, 0, 0],\
-                [0, 0, 0, 0, 0, 0, 0, 0, 0],\
-                [0, 0, 0, 0, 0, 0, 0, 0, 0],\
-                [0, 0, 0, 0, 0, 0, 0, 0, 0],\
-                [0, 0, 0, 0, 0, 0, 0, 0, 0],\
-                [0, 0, 0, 0, 0, 0, 0, 0, 0]]
+    # the numbers are placeholders, the actual list is a list of Cell objects
+    cells = []
+    origin_pts = []
 
     """
     Initializer
@@ -24,6 +18,11 @@ class SudokuSolver:
 
         file = open(f_path)
         self.is_done = False
+
+        # this adds 9 empty lists, they are the 9 y axis's
+        for i in range(9):
+            self.cells.append([])
+
         # loops for 3 squares
         for i in range(3):
             # loops for 3 rows
@@ -31,28 +30,34 @@ class SudokuSolver:
                 y_index = 3*i+j
                 line = file.readline().split()
                 for x_index in range(9):
-                    self.elements[x_index][y_index] = line[x_index]
+                    num = int(line[x_index])
+                    self.cells[y_index].append(cell.Cell(num, x_index, y_index))
+                        # This passes in the num, x_index, and y_index
+                    if num != 0:
+                        self.origin_pts.append((x_index, y_index))
             # readline to get rid of white space
             file.readline()
         # The coordinates are x axis then y axis
         self.curr = [0, 0]
+        print(self.origin_pts)
 
 
     """
     Move one step in the algorithm
     """
     def step(self):
-        cell.Cell currCell = self.elements[self.curr[0]][self.curr[1]]
+        # this is of type Cell
+        currCell = self.cells[self.curr[0]][self.curr[1]]
         
         # if the current cell is valid
         if self.is_valid(currCell): # if the cell is valid, move on
-            self.get_next(self.curr)
+            self.get_next(self.curr) # moves coordinates to next position
             if self.curr[1]>8: # this is when we've finished the last cell
                 self.is_done = True
         elif currCell.next_num() == -1: # if no valid numbers back track
-            self.get_prev(self.curr)
+            self.get_prev(self.curr) # moves coordinates to previous position
         else:
-            currCell.set_num(curCell.next_num())
+            currCell.set_num(currCell.next_num())
     
     """
     Prints the puzzle in console
@@ -62,7 +67,7 @@ class SudokuSolver:
             for j in range(3):
                 y_index = i*3 + j
                 for x_index in range(9):
-                    print(f'{SudokuSolver.elements[x_index][y_index]}', end=' ')
+                    print(f'{self.elements[y_index][x_index].get_num()}', end=' ')
                     if x_index==2 or x_index==5:
                         print("|", end=' ')
                 print("")
@@ -73,25 +78,30 @@ class SudokuSolver:
     Return if finished
     """
     def has_next(self):
-        return is_done
+        return self.is_done
         
 
-    def check_valid(cell.Cell c):
-        x = c.x_index
-        y = c.y_index
-        num = c.index+1 # converting to number rather than index
+    def is_valid(self, c: cell.Cell):
+        x = c.x
+        y = c.y
+        num = c.num
         
         # checking row
-        
+        for x_i in range(9):
+            if (x_i != x) and (self.cells[y][x_i].get_num() == num):
+                return False
 
         # checking column
-        
+        for y_i in range(9):
+            if (y_i != y) and (self.cells[y_i][x].get_num() == num):
+                return False
 
         # checking square
-        
+        # 
+        return True
 
     # gets the next coordinate, not bounded on y. coordinate changed in place
-    def get_next(coord):
+    def get_next(self, coord):
         if(coord[0] == 8):
             coord[0] = 0
             coord[1] += 1
@@ -100,7 +110,7 @@ class SudokuSolver:
         return
     
     # gets the previous coordinate. coordinate changed in place
-    def get_prev(coord):
+    def get_prev(self, coord):
         if(coord[0] == 0):
             coord[0] = 8
             coord[1] -= 1
--- cell.py
+++ cell.py
@@ -1,33 +1,37 @@
-class cell:
+class Cell:
 
     # 1 denotes possiblity, 0 denotes impossiblity
 
     def __init__(self, num, x, y):
         self.x = x
         self.y = y
-        self.i = 0 # this is the index of the current number
+        # the index will be max(num-1, 0)
+        self.num = num
+        self.is_fixed = False
 
         if num == 0:
             self.possible_nums = [1, 1, 1, 1, 1, 1, 1, 1, 1]
         else:
             self.possible_nums = [0, 0, 0, 0, 0, 0, 0, 0, 0]
             self.possible_nums[num-1] = 1
+            self.is_fixed = True
          
 
-    # returns the index not the actual number
+    # returns the next number, does not set anything
     def next_num(self):
-        for j in range(self.i+1, 9):
+        for j in range(self.num, 9):
             if self.possible_nums[j] != 0:
-                self.i = j
-                return j
+                return self.num
         return -1 # this is only if there are no possible nums
     
-    # removes number given actual number
+    # removes number given num
     def remove_num(self, num):
         self.possible_nums[num-1] = 0
 
-    # num should be given as index, not as the actual number
+    # sets the number to num
     def set_num(self, num):
-        self.i = num
-    
+        self.num = num
+
+    def get_num(self):
+        return self.num
 
